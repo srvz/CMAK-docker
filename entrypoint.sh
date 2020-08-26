08acb724cc603ccdf54902c12dfd0316b3cd5f41
default <default@local.mac>
2020-08-27 03:25:06 +0800

    fix CMAK_ARGS bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-export APPLICATION_SECRET="${APPLICATION_SECRET:-$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c 64)}"
+export APPLICATION_SECRET="${APPLICATION_SECRET:-$(LC_ALL=C tr -dc [:alnum:] < /dev/urandom | head -c 64)}"
 
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
-  set -- /app/bin/cmak "${CMAK_ARGS}" "${@}"
+  set -- /app/bin/cmak ${CMAK_ARGS} "${@}"
 fi
 
 exec "$@"
